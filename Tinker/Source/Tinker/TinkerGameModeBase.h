4128dfa49b3fd1a23ad7a0f70652ae0f84165d52
James Bradford <jrbrad44@hotmail.co.uk>
2021-05-26 13:11:33 +0100

    init stage 2

---
+++ Tinker/Source/Tinker/TinkerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TinkerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINKER_API ATinkerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
