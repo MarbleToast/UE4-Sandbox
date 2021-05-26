9c905ba3dec40533a794e19441c91cd58b7be244
James Bradford <jrbrad44@hotmail.co.uk>
2021-05-26 12:40:59 +0100

    Init project 1

---
+++ Tinker/Intermediate/Build/Win64/UE4Editor/Inc/Tinker/TinkerGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TINKER_TinkerGameModeBase_generated_h
+#error "TinkerGameModeBase.generated.h already included, missing '#pragma once' in TinkerGameModeBase.h"
+#endif
+#define TINKER_TinkerGameModeBase_generated_h
+
+#define Tinker_Source_Tinker_TinkerGameModeBase_h_15_SPARSE_DATA
+#define Tinker_Source_Tinker_TinkerGameModeBase_h_15_RPC_WRAPPERS
+#define Tinker_Source_Tinker_TinkerGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Tinker_Source_Tinker_TinkerGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATinkerGameModeBase(); \
+	friend struct Z_Construct_UClass_ATinkerGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATinkerGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Tinker"), NO_API) \
+	DECLARE_SERIALIZER(ATinkerGameModeBase)
+
+
+#define Tinker_Source_Tinker_TinkerGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATinkerGameModeBase(); \
+	friend struct Z_Construct_UClass_ATinkerGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATinkerGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Tinker"), NO_API) \
+	DECLARE_SERIALIZER(ATinkerGameModeBase)
+
+
+#define Tinker_Source_Tinker_TinkerGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATinkerGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATinkerGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATinkerGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATinkerGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATinkerGameModeBase(ATinkerGameModeBase&&); \
+	NO_API ATinkerGameModeBase(const ATinkerGameModeBase&); \
+public:
+
+
+#define Tinker_Source_Tinker_TinkerGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATinkerGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATinkerGameModeBase(ATinkerGameModeBase&&); \
+	NO_API ATinkerGameModeBase(const ATinkerGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATinkerGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATinkerGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATinkerGameModeBase)
+
+
+#define Tinker_Source_Tinker_TinkerGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Tinker_Source_Tinker_TinkerGameModeBase_h_12_PROLOG
+#define Tinker_Source_Tinker_TinkerGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Tinker_Source_Tinker_TinkerGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Tinker_Source_Tinker_TinkerGameModeBase_h_15_SPARSE_DATA \
+	Tinker_Source_Tinker_TinkerGameModeBase_h_15_RPC_WRAPPERS \
+	Tinker_Source_Tinker_TinkerGameModeBase_h_15_INCLASS \
+	Tinker_Source_Tinker_TinkerGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Tinker_Source_Tinker_TinkerGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Tinker_Source_Tinker_TinkerGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Tinker_Source_Tinker_TinkerGameModeBase_h_15_SPARSE_DATA \
+	Tinker_Source_Tinker_TinkerGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Tinker_Source_Tinker_TinkerGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Tinker_Source_Tinker_TinkerGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TINKER_API UClass* StaticClass<class ATinkerGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Tinker_Source_Tinker_TinkerGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
